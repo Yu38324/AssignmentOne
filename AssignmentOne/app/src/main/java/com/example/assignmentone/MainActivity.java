7aed2cd5832ce39d2d727a5cd7574dad718138ab
Yu38324 <stacieyu@163.com>
2025-11-02 16:40:37 +0800

    add Toast and onclick event

--- AssignmentOne/app/src/main/java/com/example/assignmentone/MainActivity.java
+++ AssignmentOne/app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.assignmentone;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +24,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    //facilities
+    public void onFacilitiesClick(View view){
+        Toast.makeText(this,"Facilities clicked", Toast.LENGTH_SHORT).show();
+    }
+    //events
+    public void onEventsClick(View view) {
+        Toast.makeText(this, "Events clicked", Toast.LENGTH_SHORT).show();
+    }
+    //club
+    public void onClubsClick(View view) {
+        Toast.makeText(this, "Clubs clicked", Toast.LENGTH_SHORT).show();
+    }
+    //support
+    public void onSupportClick(View view) {
+        Toast.makeText(this, "Support clicked", Toast.LENGTH_SHORT).show();
+    }
+
 }
